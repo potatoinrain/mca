b442684dea3d4aae66a583d4ea4aa01884bff590
yangy <potatoinrain@126.com>
2022-05-17 23:38:17 +0800

    create

---
+++ src/main/java/log/logback.java
@@ -0,0 +1,12 @@
+package log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class logback {
+    protected static final Logger logger = LoggerFactory.getLogger(logback.class);
+
+    public static void main(String[] args) {
+        logger.info("test");
+    }
+}
